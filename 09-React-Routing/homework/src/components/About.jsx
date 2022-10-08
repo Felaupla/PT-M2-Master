355f2c085e50d2ac3a6870c769747a041054a59c
Felipe Planes <felaupla@MacBook-Pro-de-Felipe.local>
2022-10-07 22:02:04 -0300

    homework09-ReactRouting

---
+++ 09-React-Routing/homework/src/components/About.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+export default function About(){
+    return (
+        <div>
+            <h1>FELIPE PLANES</h1>
+            <h3>MASTERCARD</h3>
+            <h3>5388-4455-8899-1234</h3>
+        </div>
+    )
+}
--- 09-React-Routing/homework/src/containers/App.js
+++ 09-React-Routing/homework/src/containers/App.js
@@ -1,22 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { Route } from "react-router-dom";
 
-import './App.css';
-import Nav from '../components/Nav.jsx';
-import Cards from '../components/Cards.jsx';
+import "./App.css";
+import Nav from "../components/Nav.jsx";
+import Cards from "../components/Cards.jsx";
+import About from "../components/About";
+import Ciudad from "../components/Ciudad";
 
-const apiKey = 'Aqui va la API key que creaste';
+const apiKey = "4ae2636d8dfbdc3044bede63951a019b";
 
 function App() {
   const [cities, setCities] = useState([]);
   function onClose(id) {
-    setCities(oldCities => oldCities.filter(c => c.id !== id));
+    setCities((oldCities) => oldCities.filter((c) => c.id !== id));
   }
   function onSearch(ciudad) {
     //Llamado a la API del clima
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}`)
-      .then(r => r.json())
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&units=metric`
+    )
+      .then((r) => r.json())
       .then((recurso) => {
-        if(recurso.main !== undefined){
+        if (recurso.main !== undefined) {
           const ciudad = {
             min: Math.round(recurso.main.temp_min),
             max: Math.round(recurso.main.temp_max),
@@ -28,31 +33,44 @@ function App() {
             weather: recurso.weather[0].main,
             clouds: recurso.clouds.all,
             latitud: recurso.coord.lat,
-            longitud: recurso.coord.lon
+            longitud: recurso.coord.lon,
           };
-          setCities(oldCities => [...oldCities, ciudad]);
+          setCities((oldCities) => [...oldCities, ciudad]);
         } else {
           alert("Ciudad no encontrada");
         }
       });
   }
   function onFilter(ciudadId) {
-    let ciudad = cities.filter(c => c.id === parseInt(ciudadId));
-    if(ciudad.length > 0) {
-        return ciudad[0];
+    let ciudad = cities.filter((c) => c.id === parseInt(ciudadId));
+    if (ciudad.length > 0) {
+      return ciudad[0];
     } else {
-        return null;
+      return null;
     }
   }
   return (
     <div className="App">
-      <Nav onSearch={onSearch}/>
-      <div>
-        <Cards
-          cities={cities}
-          onClose={onClose}
-        />
-      </div>
+      {/* ruta nav*/}
+      <Route path="/" render={() => <Nav onSearch={onSearch} />} />
+      {/* ruta home*/}
+      <Route
+        exact
+        path="/"
+        render={() => {
+          return <Cards cities={cities} onClose={onClose} />;
+        }}
+      />
+      {/* ruta about*/}
+      <Route path="/about" component={About} />
+
+      {/* ruta ciudad*/}
+      <Route
+        path="/ciudad/:id"
+        render={({ match }) => {
+          return <Ciudad city={onFilter(match.params.id)} />;
+        }}
+      />
       <hr />
     </div>
   );
